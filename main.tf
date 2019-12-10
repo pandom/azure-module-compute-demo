ab4c75d5fbf86690465881b3fe61c843a9ee04ef
Anthony Burke <burkey@hashicorp.com>
2019-12-10 11:45:26 +1100

    intial network

--- main.tf
+++ main.tf
@@ -1,35 +1,15 @@
-  module "linuxservers" {
-    source              = "Azure/compute/azurerm"
-    location            = "West US 2"
-    vm_os_simple        = "UbuntuServer"
-    public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
-    vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-  }
-
-  module "windowsservers" {
-    source              = "Azure/compute/azurerm"
-    location            = "West US 2"
-    vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-    admin_password      = "ComplxP@ssw0rd!"
-    vm_os_simple        = "WindowsServer"
-    is_windows_image    = "true"
-    public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
-    vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-  }
-
-  module "network" {
+module "network" {
     source              = "Azure/network/azurerm"
-    version             = "~> 1.1.1"
-    location            = "West US 2"
-    allow_rdp_traffic   = "true"
-    allow_ssh_traffic   = "true"
-    resource_group_name = "terraform-compute"
-  }
+    resource_group_name = "myapp"
+    location            = "australiaeast"
+    address_space       = "10.0.0.0/16"
+    subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
-  output "linux_vm_public_name"{
-    value = "${module.linuxservers.public_ip_dns_name}"
-  }
+    tags                = {
+                            environment = "production"
+                            costcenter  = "it"
+                            owner  = "beca"
 
-  output "windows_vm_public_name"{
-    value = "${module.windowsservers.public_ip_dns_name}"
-  }
+                          }
+}
